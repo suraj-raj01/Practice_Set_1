010ee7b5fb97b100029aecea9ea6d3a3b350bfe1
SURAJ_KUMAR <surajk38656@gmail.com>
2024-10-16 22:50:25 +0530

    lenght of last string

---
+++ src/LengthOfLastString/LengthLastString.java
@@ -0,0 +1,15 @@
+package LengthOfLastString;
+
+public class LengthLastString {
+    public static int lengthOfLastString(String s){
+        s = s.trim();
+        int count=0;
+        for(int i=s.length()-1; i>=0; i--){
+            if(s.charAt(i)==' '){
+                break;
+            }
+            count++;
+        }
+        return count;
+    }
+}
---
+++ src/LengthOfLastString/Main.java
@@ -0,0 +1,9 @@
+package LengthOfLastString;
+// leetCode Problems-58 : Length of Last Word
+public class Main {
+    public static void main(String[] args) {
+        String s = " A very beautiful lady ";
+        System.out.print("Length of Last String : ");
+        System.out.println(LengthLastString.lengthOfLastString(s));
+    }
+}
